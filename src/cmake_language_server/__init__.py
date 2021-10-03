76e34ae6287f6b4db8e397330a73659d9f2aa7ee
Regen <regen100@users.noreply.github.com>
2021-10-03 12:51:16 +0900

    Fix __version__ (#43)

--- src/cmake_language_server/__init__.py
+++ src/cmake_language_server/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.2"
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
